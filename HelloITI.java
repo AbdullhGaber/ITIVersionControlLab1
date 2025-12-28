a30127c4032d557733bb69414fc41ec443ba554b
AbdullhGaber <abdullh760@yahoo.com>
2025-12-28 12:37:46 +0200

    Modify HelloITI.java and add printHelloGit method

--- HelloITI.java
+++ HelloITI.java
@@ -2,4 +2,8 @@ public class HelloITI {
     public static void main(String[] args) {
         System.out.println("HelloITI");
     }
+
+    public static void printHelloGit() {
+         System.out.println("Hello Git");
+    }
 }
